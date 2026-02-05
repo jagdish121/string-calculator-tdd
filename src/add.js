3caa765fe9383cc9f6c465f0485c03c0f373f342
jagdish121 <jjhiont121@gmail.com>
2026-02-05 21:16:43 +0530

    test: handle single number input
    test: handle single number input

--- src/add.js
+++ src/add.js
@@ -1,5 +1,6 @@
 function add(numbers) {
-  return 0;
+  if (numbers === "") return 0;
+  return Number(numbers);
 }
 
 module.exports = add;
--- test/add.test.js
+++ test/add.test.js
@@ -3,3 +3,7 @@ const add = require("../src/add");
 test("returns 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns number for single value", () => {
+  expect(add("1")).toBe(1);
+});
