3c91e7286ebe363c818f271fc7c0e50ec91c6afa
jagdish121 <jjhiont121@gmail.com>
2026-02-05 21:21:31 +0530

    test: support unknown amount of numbers
    test: support unknown amount of numbers

--- test/add.test.js
+++ test/add.test.js
@@ -11,3 +11,7 @@ test("returns number for single value", () => {
 test("returns sum of two comma separated numbers", () => {
   expect(add("1,2")).toBe(3);
 });
+
+test("handles unknown amount of numbers", () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
