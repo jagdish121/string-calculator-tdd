6b35b303d387d01d4fd73a4e310fd72bc5ff07a2
jagdish121 <jjhiont121@gmail.com>
2026-02-05 21:50:30 +0530

    test: support single custom delimiter
    test: support single custom delimiter

--- src/add.js
+++ src/add.js
@@ -1,8 +1,16 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
+  let delimiter = /,|\n/;
+
+  if (numbers.startsWith("//")) {
+    const [header, rest] = numbers.split("\n");
+    numbers = rest;
+    delimiter = header[2];
+  }
+  
   return numbers
-  .split(/,|\n/)
+  .split(delimiter)
   .map(Number)
   .filter(n => n <= 1000)
   .reduce((a, b) => a + b, 0);
